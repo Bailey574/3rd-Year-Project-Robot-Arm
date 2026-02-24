e0d8f1b96b430fa587f6a135ecbf77f7de978fe6
Bailey574 <159044641+Bailey574@users.noreply.github.com>
2026-02-24 12:26:39 +0000

    Update DistanceTesting.ino

--- DistanceTesting.ino
+++ DistanceTesting.ino
@@ -1,11 +1,11 @@
-// --- TCS3200 Colour Sensor Pins ---
+
 #define S0 2
 #define S1 4
 #define S2 7
 #define S3 8
 #define OUT 12
 
-// --- HC-SR04 Distance Sensor Pins ---
+
 #define TRIG_PIN 13
 #define ECHO_PIN A0  // analog pin used as digital input
 
@@ -19,18 +19,18 @@ int distance;
 void setup() {
   Serial.begin(9600);
 
-  // --- Colour sensor setup ---
+  
   pinMode(S0, OUTPUT);
   pinMode(S1, OUTPUT);
   pinMode(S2, OUTPUT);
   pinMode(S3, OUTPUT);
   pinMode(OUT, INPUT);
 
-  // Set 20% frequency scaling
+  
   digitalWrite(S0, HIGH);
   digitalWrite(S1, LOW);
 
-  // --- Distance sensor setup ---
+  
   pinMode(TRIG_PIN, OUTPUT);
   pinMode(ECHO_PIN, INPUT);
 
@@ -38,26 +38,25 @@ void setup() {
 }
 
 void loop() {
-  // ------------------ Colour Sensor ------------------
-  // RED
+  
   digitalWrite(S2, LOW);
   digitalWrite(S3, LOW);
   delay(50);
   redFreq = pulseIn(OUT, LOW);
 
-  // GREEN
+  
   digitalWrite(S2, HIGH);
   digitalWrite(S3, HIGH);
   delay(50);
   greenFreq = pulseIn(OUT, LOW);
 
-  // BLUE
+  
   digitalWrite(S2, LOW);
   digitalWrite(S3, HIGH);
   delay(50);
   blueFreq = pulseIn(OUT, LOW);
 
-  // Classification (RED, GREEN, BLUE only)
+  
   String detectedColor = "Unknown";
 
   if (redFreq < greenFreq && redFreq < blueFreq) {
@@ -70,21 +69,20 @@ void loop() {
       detectedColor = "BLUE";
   }
 
-  // ------------------ Distance Sensor ------------------
-  // Trigger pulse
+  
   digitalWrite(TRIG_PIN, LOW);
   delayMicroseconds(2);
   digitalWrite(TRIG_PIN, HIGH);
   delayMicroseconds(10);
   digitalWrite(TRIG_PIN, LOW);
 
-  // Read echo pulse duration
+  
   duration = pulseIn(ECHO_PIN, HIGH);
 
-  // Calculate distance in cm
+  
   distance = duration * 0.034 / 2;
 
-  // ------------------ Print Results ------------------
+  
   Serial.print("R: "); Serial.print(redFreq);
   Serial.print(" G: "); Serial.print(greenFreq);
   Serial.print(" B: "); Serial.print(blueFreq);
@@ -92,5 +90,5 @@ void loop() {
   Serial.print(" | Distance: "); Serial.print(distance);
   Serial.println(" cm");
 
-  delay(300); // short pause before next reading
+  delay(300); 
 }
